553aafe5ffb8467a011d2386ea5d87c42b7bb7bb
Lana Ferrari <ferrarirangel@gmail.com>
2022-02-11 09:33:31 +0000

    Adding filters

--- src/containers/cardlist.jsx
+++ src/containers/cardlist.jsx
@@ -8,12 +8,31 @@ const Cardlist = (props) => {
   return (
     <div className="container">
       {beersArray.map((beer, index) => {
+        console.log(beer);
         const description = beer.description.split(".");
         return (
           <div key={index} className="container__item">
-            <img src={beerPic} alt={beer.name} width="200px" />
-            <h3>{beer.name}</h3>
-            <p>{`${description[0]}.`}</p>
+            <img
+              className="container__img"
+              src={beerPic}
+              alt={beer.name}
+              width="200px"
+            />
+            <div className="container__tittle-container">
+              <h3 className="container__tittle-container__title">
+                {beer.name}
+              </h3>
+            </div>
+            <p className="container__paragraph">{`${
+              description[0].length >= 50
+                ? description[0]
+                : description[0] + description[1]
+            }.`}</p>
+
+            <div className="container__about">
+              <p className="container__about__ph">ph: {beer.ph}</p>
+              <p className="container__about__abv">ABV: {beer.abv}</p>
+            </div>
           </div>
         );
       })}
