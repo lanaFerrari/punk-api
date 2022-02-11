7ceb0500d275a845a7a79f30c523459cfb3f0e01
Lana Ferrari <ferrarirangel@gmail.com>
2022-02-11 09:34:16 +0000

    Add navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,31 +2,40 @@ import React from "react";
 import "./navbar.scss";
 
 const Navbar = (props) => {
-  const { filterByPh, filterByRange, filterByAbv } = props;
+  const { filterByPh, filterByRange, filterByAbv, userInput, handleInput } =
+    props;
 
   return (
     <div className="navbar">
       <div className="navbar__textInput">
-        <input type="text" id="input" name="input" placeholder="Search" />
-      </div>
-
-      <div className="navbar__check-input-abv">
-        <label htmlFor="abv">{`HIGH ABV(> 6%)`}</label>
-        <input onClick={filterByAbv} type="checkbox" id="abv" name="abv" />
-      </div>
-      <div className="navbar__check-input-range">
-        <label htmlFor="range">{`Classic Range`}</label>
         <input
-          onClick={filterByRange}
-          type="checkbox"
-          id="range"
-          name="range"
+          type="text"
+          id="input"
+          name="input"
+          value={userInput}
+          placeholder="Search for a beer here or click in the options below"
+          onChange={handleInput}
         />
       </div>
-      <div className="navbar__check-input-ph">
-        <label htmlFor="ph">{`Acidic(pH < 4)`}</label>
-        <input onClick={filterByPh} type="checkbox" id="ph" name="ph" />
-      </div>
+      <section className="navbar__section">
+        <div className="navbar__check-input-range">
+          <label htmlFor="range">{`Classic Range`}</label>
+          <input
+            onClick={filterByRange}
+            type="checkbox"
+            id="range"
+            name="range"
+          />
+        </div>
+        <div className="navbar__check-input-ph">
+          <label htmlFor="ph">{`Acidic (ph < 4)`}</label>
+          <input onClick={filterByPh} type="checkbox" id="ph" name="ph" />
+        </div>
+        <div className="navbar__check-input-abv">
+          <label htmlFor="abv">{`High ABV`}</label>
+          <input onClick={filterByAbv} type="checkbox" id="abv" name="abv" />
+        </div>
+      </section>
     </div>
   );
 };
