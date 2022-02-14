08400b811932d9bc051a24cee7ca3719321574cc
Lana Ferrari <ferrarirangel@gmail.com>
2022-02-14 11:21:08 +0000

    Fixing typos

--- src/components/HomeMain/HomeMain.jsx
+++ src/components/HomeMain/HomeMain.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../Navbar/Navbar";
-import CardList from "../../containers/CardList";
 import "./HomeMain.scss";
+import Cardlist from "../../containers/Cardlist";
 
 const HomeMain = (props) => {
   const { beersList } = props;
@@ -51,7 +51,7 @@ const HomeMain = (props) => {
         userInput={userInput}
         handleInput={handleInput}
       />
-      <CardList beersArray={filteredBeerList} />
+      <Cardlist beersArray={filteredBeerList} />
     </main>
   );
 };
